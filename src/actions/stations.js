f8a77d425a819d4f8ed0b0058d6ca60b518fca66
Raphael Gruber <raphi011@gmail.com>
2016-12-28 17:10:39 +0100

    adds buildurl method for api urls

--- src/actions/stations.js
+++ src/actions/stations.js
@@ -2,7 +2,28 @@ import fetch from 'isomorphic-fetch';
 
 const apiConfig = require('../../transport-api');
 
-const apiUrl = `https://transportapi.com/v3/uk/train/stations/near.json?app_id=${apiConfig.appid}&app_key=${apiConfig.key}&lat=${apiConfig.lat}&lon=${apiConfig.lon}`;
+// const apiUrl = 'https://transportapi.com/v3/uk/train/';
+
+const lat = '51.507513';
+const lon = '-0.126971';
+
+const apiUrl = `https://transportapi.com/v3/uk/train/stations/near.json?app_id=${apiConfig.app_id}&app_key=${apiConfig.app_key}&lat=${lat}&lon=${lon}`;
+
+// train/stations/near.json?app_id=${apiConfig.appid}&app_key=${apiConfig.key}&lat=${apiConfig.lat}&lon=${apiConfig.lon}`;
+
+/*
+const buildUrl = (config, resourceUrl, params) => {
+  let url = `${apiUrl}${resourceUrl}?app_id=${config.app_id}&app_key=${config.app_key}`;
+
+};
+
+
+
+const nearApiUrl = buildUrl(apiConfig, 'stations/near.json', {
+  lat: "51.507513",
+  "lon": "-0.126971",
+});
+*/
 
 export const requestStations = () => ({
   type: 'REQUEST_STATIONS',
